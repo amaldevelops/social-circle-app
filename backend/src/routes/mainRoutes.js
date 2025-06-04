9f4351c5e6e7e55a18e3ec74a97f90ddb22645ad
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-06-04 11:14:36 +1000

    Update Comments

--- backend/src/routes/mainRoutes.js
+++ backend/src/routes/mainRoutes.js
@@ -18,20 +18,23 @@ const mainRouter = Router();
 mainRouter.get("/social-circle-api/v1/status", apiStatus); // End Point to show the running status of backend systems
 
 mainRouter.post("/social-circle-api/v1/profile/login", login); // POST End Point to login to Social Circle App
+//Requirement: Users must sign in to see anything except the sign-in page
 
 mainRouter.post("/social-circle-api/v1/profile/register", register); // POST End Point to Register new user
+
+mainRouter.get(
+  "/social-circle-api/v1/profile/:loggedInUserID/messages",
+  authenticateJWT,
+  getContactMessages
+); // Authenticated End Point to GET users profile
+//Requirement: A user’s profile page should contain their profile information, profile photo, and posts
 
 mainRouter.get(
   "/social-circle-api/v1/profile",
   authenticateJWT,
   getAllContacts
 ); // Authenticated End Point to GET all the
-
-mainRouter.get(
-  "/social-circle-api/v1/profile/:loggedInUserID/messages",
-  authenticateJWT,
-  getContactMessages
-); // Authenticated End Point to GET messages sent and received by the logged in user
+//Requirement: There should be an index page for users, which shows all users and buttons for sending follow requests to users the user is not already following or have a pending request.
 
 mainRouter.post(
   "/social-circle-api/v1/profile/:loggedInUserID/message/:contactID",
@@ -50,15 +53,14 @@ mainRouter.put(
   authenticateJWT,
   updateLoggedUserProfile
 ); // Authenticated End Point to update logged in users profile
+//Requirement: Users can create a profile with a profile picture.
 
-//Users can send follow requests to other users.
-//Users can create posts (begin with text only)
-//Users can like posts
-//Users can comment on posts
-//Posts should always display the post content, author, comments, and likes
-//There should be an index page for posts, which shows all the recent posts from the current user and users they are following.
-//Users can create a profile with a profile picture.
-//A user’s profile page should contain their profile information, profile photo, and posts
-//There should be an index page for users, which shows all users and buttons for sending follow requests to users the user is not already following or have a pending request.
+
+//Requirement: Users can send follow requests to other users.
+//Requirement: Users can create posts (begin with text only)
+//Requirement: Users can like posts
+//Requirement: Users can comment on posts
+//Requirement: Posts should always display the post content, author, comments, and likes
+//Requirement: There should be an index page for posts, which shows all the recent posts from the current user and users they are following.
 
 export default mainRouter;
