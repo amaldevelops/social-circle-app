af076291c98668c747cab4964e0e1cd9d6e9fb1f
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-06-06 18:15:47 +1000

    Add demo account login functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,30 @@ import ProjectInfo from "./components/ProjectInfo";
 import JWTStatus from "./components/JwtStatus";
 import { useNavigate } from "react-router-dom";
 
+import { ApiLogin } from "./ApiQueries"; // Assuming ApiLogin is an async function
+
 import { Outlet } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    email: "demoaccount@amalk.au",
+    password: "Demo2025",
+  });
+
   const Navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    // Make the function asynchronous
+
+    await ApiLogin(formData); // Await the API login call to complete
+
+    // Add a 2-second delay before navigating
+    setTimeout(() => {
+      Navigate("/social-circle-app/socialfeed");
+    }, 2000); // 2000 milliseconds = 2 seconds
+  };
 
   return (
     <>
@@ -19,12 +37,16 @@ function App() {
         <button className="button-container" onClick={() => Navigate("login")}>
           Login
         </button>
+        <button className="button-container" onClick={() => handleSubmit()}>
+          Demo Account login
+        </button>
         <button
           className="button-container"
           onClick={() => Navigate("register")}
         >
           Register
         </button>
+
         <Outlet />
       </div>
     </>
